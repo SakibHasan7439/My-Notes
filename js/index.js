660399aeeaeb4c7b03336012e405219fceee608e
sakibHasan7439 <sakibhasanb120@gmail.com>
2024-09-28 15:01:36 +0600

    first part done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 const searchField = document.getElementById("search");
+const sectionContainer = document.getElementById("section-list");
+const textArea = document.getElementById("takeNote");
+const noteContainer = document.getElementById("note-container");
+const button = document.getElementById("button");
+
+button.addEventListener("click", ()=>{
+    const div = document.createElement("div");
+    div.className = "bg-green-500 text-white rounded-md shadow-md p-4"
+    const p = document.createElement("p");
+    p.innerText = textArea.value;
+    div.appendChild(p);
+    noteContainer.append(div);
+
+    textArea.value = "";
+})
